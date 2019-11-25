033c19b443c9db3f7c26c1fcabeddd570d93edee
LBrinkmann <com@levin-brinkmann.de>
2019-11-25 14:05:33 +0100

    xserver dummy

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 DOCKER_ACC=levinb
 DOCKER_REPO=mctest
-IMG_TAG=tf2
+IMG_TAG=tf3
 
 docker build -t $DOCKER_ACC/$DOCKER_REPO:$IMG_TAG -f "./Dockerfile.tensorflow" "."
 docker push $DOCKER_ACC/$DOCKER_REPO:$IMG_TAG
